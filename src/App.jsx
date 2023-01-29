e817a99f6e55e6b695a98e2d96f9cf914dded6cc
vaperLee <rize1126jesse@gmail.com>
2023-01-29 12:02:54 +0900

    tmp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProgramDescription from "./ProgramDescription";
+
+const App = () => {
+  return (
+    <div>
+      <ProgramDescription content={"this is react"} />
+    </div>
+  );
+};
+
+export default App;
